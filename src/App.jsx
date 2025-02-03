0c5ef5c3e3a131108931fdeae8d8d5a4ae3e733d
Rupesh Siva Sai <105511559+rupeshsivasai@users.noreply.github.com>
2025-02-03 21:16:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,32 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/quotes/')
+    .then(response => response.json())
+    .then(data => setQuotes(data.quotes))
+    .catch(error => console.error('Error Fetching Data', error))
+  },[])
+
+  let list = '📃'
+  let stars = '✨⭐🌟'
+ 
   return (
-    <div>
-      
+    <div className='App' >
+      <h1>{list} Quotes List {stars}</h1>
+
+      <div className='quotes-container' >
+
+      {quotes.map(quote => (
+        <div key={quote.id} className="quote-card">
+          <p className="quote-text">"{quote.quote}"</p>
+          <p className="quote-author">- {quote.author}</p>
+        </div>
+      ))}
+      </div>
+
     </div>
   )
 }
